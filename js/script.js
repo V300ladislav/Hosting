b3bd456e4b6716c3f931aa52269b293c2fbce476
V300ladislav <67784810+V300ladislav@users.noreply.github.com>
2020-07-10 10:44:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        dots: false,
+        speed:1500,
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        dots: true
+    });
+  });
